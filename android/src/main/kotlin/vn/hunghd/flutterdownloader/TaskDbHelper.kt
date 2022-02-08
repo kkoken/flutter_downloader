f1769bf01e9bc3366f16eb65abcdde985be0030c
Kaan Taha Köken <kaankoken@gain.com.tr>
2022-02-08 14:10:43 +0300

    Fix sql query

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
@@ -24,25 +24,26 @@ class TaskDbHelper private constructor(context: Context) :
         const val DATABASE_VERSION = 3
         const val DATABASE_NAME = "download_tasks.db"
         private var instance: TaskDbHelper? = null
-        private val SQL_CREATE_ENTRIES = ("CREATE TABLE " + TaskEntry.TABLE_NAME + " (" +
-                "PRIMARY KEY (" + TaskEntry().id + "), " +
-                TaskEntry.COLUMN_NAME_TASK_ID + " VARCHAR(256), " +
-                TaskEntry.COLUMN_NAME_URL + " TEXT, " +
-                TaskEntry.COLUMN_NAME_STATUS + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_PROGRESS + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_FILE_NAME + " TEXT, " +
-                TaskEntry.COLUMN_NAME_SAVED_DIR + " TEXT, " +
-                TaskEntry.COLUMN_NAME_HEADERS + " TEXT, " +
-                TaskEntry.COLUMN_NAME_MIME_TYPE + " VARCHAR(128), " +
-                TaskEntry.COLUMN_NAME_RESUMABLE + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION + " TINYINT DEFAULT 0, " +
-                TaskEntry.COLUMN_NAME_TIME_CREATED + " INTEGER DEFAULT 0, " +
-                TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0"
-                + ")")
+        private const val SQL_CREATE_ENTRIES = (
+                "CREATE TABLE " + TaskEntry.TABLE_NAME + "(" +
+                        TaskEntry.ID + " INTEGER," +
+                        TaskEntry.COLUMN_NAME_TASK_ID + " VARCHAR(256)," +
+                        TaskEntry.COLUMN_NAME_URL + " TEXT," +
+                        TaskEntry.COLUMN_NAME_STATUS + " INTEGER DEFAULT 0," +
+                        TaskEntry.COLUMN_NAME_PROGRESS + " INTEGER DEFAULT 0," +
+                        TaskEntry.COLUMN_NAME_FILE_NAME + " TEXT," +
+                        TaskEntry.COLUMN_NAME_SAVED_DIR + " TEXT," +
+                        TaskEntry.COLUMN_NAME_HEADERS + " TEXT," +
+                        TaskEntry.COLUMN_NAME_MIME_TYPE + " VARCHAR(128)," +
+                        TaskEntry.COLUMN_NAME_RESUMABLE + "  TINYINT DEFAULT 0," +
+                        TaskEntry.COLUMN_NAME_SHOW_NOTIFICATION + " TINYINT DEFAULT 0," +
+                        TaskEntry.COLUMN_NAME_OPEN_FILE_FROM_NOTIFICATION + " TINYINT DEFAULT 0," +
+                        TaskEntry.COLUMN_NAME_TIME_CREATED + " INTEGER DEFAULT 0," +
+                        TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0," +
+                        "PRIMARY KEY(${TaskEntry.ID}))"
+                )
         private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TaskEntry.TABLE_NAME
         fun getInstance(ctx: Context): TaskDbHelper? {
-
             // Use the application context, which will ensure that you
             // don't accidentally leak an Activity's context.
             // See this article for more information: http://bit.ly/6LRzfx
